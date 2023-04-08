eb77f87c54a91be1f7f79c1520e1f3f4561f58d5
Aidan Keenan <aidankeenan7137@gmail.com>
2023-04-08 16:29:54 -0700

    added routes and view for home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# ApplicationController is the base controller for all controllers in the application.
+# It provides common methods and functionality that are shared across multiple controllers.
 class ApplicationController < ActionController::Base
 end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# This controller handles the pages that make up the public-facing website.
+class PagesController < ApplicationController
+  def home; end
+
+  def dashboard; end
+end
