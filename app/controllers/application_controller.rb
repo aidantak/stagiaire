93df019345150e2a64c2a2c759062239d9e57752
Aidan Keenan <aidankeenan7137@gmail.com>
2023-04-17 14:26:13 -0700

    adding my routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 # ApplicationController is the base controller for all controllers in the application.
 # It provides common methods and functionality that are shared across multiple controllers.
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  def after_sign_in_path_for(_resource)
+    authenticated_root_path
+  end
+
+  def after_sign_up_path_for(_resource)
+    authenticated_root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'stages/index'
+  get 'stages/show'
+  get 'dashboard/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "pages#home"
+  authenticated :user do
+    root 'pages#dashboard', as: :authenticated_root
+  end
+
+  root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard', as: :dashboard
+  get '/users/:id', to: 'profiles#show', as: :user_profile
+  resources :stages, only: [:index, :show]
 end
